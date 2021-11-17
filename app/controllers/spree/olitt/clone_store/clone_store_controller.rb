587bc6efdc795b73baec26d0a1388e0387e417be
Stephen Gachoka <stephengachoka57@gmail.com>
2021-11-17 09:18:55 +0300

    var

--- app/controllers/spree/olitt/clone_store/clone_store_controller.rb
+++ app/controllers/spree/olitt/clone_store/clone_store_controller.rb
@@ -6,12 +6,9 @@ module Spree
       class CloneStoreController < Spree::Api::V2::BaseController # rubocop:disable Metrics/ClassLength , rubocop:disable Style/ClassVars
         include Spree::Olitt::CloneStore::CloneStoreHelpers
 
-        @@old_store = nil
-        @@new_store = nil
-
         # For Testing Only
         def test
-          @@old_store = Spree::Store.find_by(id: source_id_param)
+          @old_store = Spree::Store.find_by(id: source_id_param)
           # @new_store = Spree::Store.find_by(id: 6)
           # new_taxonomy = @new_store.taxonomies.find_by(id: 20)
           # handle_clone_taxons(new_taxonomy)
@@ -30,7 +27,7 @@ module Spree
 
         # Store
         def handle_clone_store
-          @@old_store = Spree::Store.find_by(id: source_id_param)
+          @old_store = Spree::Store.find_by(id: source_id_param)
           raise ActiveRecord::RecordNotFound if @old_store.nil?
 
           store = clone_and_update_store @old_store.dup
@@ -38,7 +35,7 @@ module Spree
         rescue StandardError => e
           Rails.logger.error(e.message)
           # render json: e.message
-          @@new_store = store
+          @new_store = store
         end
 
         def clone_and_update_store(store)
@@ -54,20 +51,21 @@ module Spree
         # Taxonomies
 
         def handle_clone_taxonomies
-          taxonomies = @@old_store.taxonomies.all
-          cloned_taxonomies = @@new_store.taxonomies.build(get_model_hash(taxonomies))
+          @old_store = Spree::Store.find_by(id: source_id_param)
+          taxonomies = @old_store.taxonomies.all
+          cloned_taxonomies = @new_store.taxonomies.build(get_model_hash(taxonomies))
           save_models(cloned_taxonomies)
         end
 
         # Taxons
 
         def handle_clone_taxons
-          taxonomies = @@new_store.taxonomies.all
+          taxonomies = @new_store.taxonomies.all
           taxonomies.each { |taxonomy| clone_taxon(taxonomy) }
         end
 
         def clone_taxon(taxonomy)
-          root_taxons = @@old_store.taxonomies.find_by(name: taxonomy.name).taxons.where(parent: nil)
+          root_taxons = @old_store.taxonomies.find_by(name: taxonomy.name).taxons.where(parent: nil)
           cloned_root_taxons = clone_update_root_taxon(root_taxons, taxonomy)
           save_models(cloned_root_taxons)
 
@@ -109,19 +107,19 @@ module Spree
         # Menus
 
         def clone_menus
-          menus = @@old_store.menus.all
+          menus = @old_store.menus.all
           raise ActiveRecord::RecordNotFound if menus.nil?
 
-          cloned_menus = @@new_store.menus.build(get_model_hash(menus))
+          cloned_menus = @new_store.menus.build(get_model_hash(menus))
           save_models(cloned_menus)
         end
 
         # Menu items
         def clone_menu_items
-          menu_items = @@old_store.menu_items.all
+          menu_items = @old_store.menu_items.all
           raise ActiveRecord::RecordNotFound if menu_items.nil?
 
-          cloned_menu_items = @@new_store.menu_items.build(get_model_hash(menu_items))
+          cloned_menu_items = @new_store.menu_items.build(get_model_hash(menu_items))
           save_models(cloned_menu_items)
         end
 
@@ -131,52 +129,52 @@ module Spree
           clone_option_types
           clone_prototypes
           clone_variants
-          products = @@old_store.products.all
-          cloned_products = @@new_store.products.build(get_model_hash(products))
+          products = @old_store.products.all
+          cloned_products = @new_store.products.build(get_model_hash(products))
           save_models(cloned_products)
         end
 
         # Products variants
 
         def clone_variants
-          variants = @@old_store.variants.all
+          variants = @old_store.variants.all
           raise ActiveRecord::RecordNotFound if variants.nil?
 
-          cloned_variants = @@new_store.products.build(get_model_hash(variants))
+          cloned_variants = @new_store.products.build(get_model_hash(variants))
           save_models(cloned_variants)
         end
 
         # Products Option Types
 
         def clone_option_types
-          raise ActiveRecord::RecordNotFound if @@old_store.option_types.all?
+          raise ActiveRecord::RecordNotFound if @old_store.option_types.all?
 
-          @option_types = @@old_store.option_types.all
+          @option_types = @old_store.option_types.all
 
-          cloned_option_types = @@new_store.option_type.option_values.build(get_model_hash(@option_types))
+          cloned_option_types = @new_store.option_type.option_values.build(get_model_hash(@option_types))
           save_models(cloned_option_types)
         end
 
         # CMS Pages
 
         def clone_cms_page
-          cms_page = @@old_store.cms_pages.all
-          cloned_cms_pages = @@new_store.cms_pages.build(get_model_hash(cms_page))
+          cms_page = @old_store.cms_pages.all
+          cloned_cms_pages = @new_store.cms_pages.build(get_model_hash(cms_page))
           save_models(cloned_cms_pages)
         end
 
         # CMS Sections
 
         def clone_cms_sections
-          cms_sections = @@old_store.cms_sections.all
-          cloned_cms_section = @@new_store.cms_sections.build(get_model_hash(cms_sections))
+          cms_sections = @old_store.cms_sections.all
+          cloned_cms_section = @new_store.cms_sections.build(get_model_hash(cms_sections))
           save_models(cloned_cms_section)
         end
 
         # Finish lifecycle
 
         def finish
-          render_serialized_payload(201) { serialize_resource(@@new_store) }
+          render_serialized_payload(201) { serialize_resource(@new_store) }
         end
       end
     end
