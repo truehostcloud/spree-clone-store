a31f205c98506bbe870d9394c65ba5012874702b
Collins Lagat <cklagat@gmail.com>
2021-11-18 12:08:55 +0300

    feast: clone menus

--- app/controllers/spree/olitt/clone_store/clone_store_controller.rb
+++ app/controllers/spree/olitt/clone_store/clone_store_controller.rb
@@ -10,9 +10,10 @@ module Spree
         # For Testing Only
         def test
           @old_store = Spree::Store.find_by(id: source_id_param)
-          @new_store = Spree::Store.find_by(id: 6)
-          handle_clone_taxons
-          render json: @new_store.taxons.all
+          @new_store = Spree::Store.find_by(id: 4)
+          return unless handle_clone_menus
+
+          render json: @new_store.menus.all
         end
 
         def clone
@@ -61,18 +62,24 @@ module Spree
 
         def handle_clone_taxons
           old_root_taxons = @old_store.taxons.where(parent: nil)
-          old_root_taxons.each { |root_taxon| clone_taxon(root_taxon) }
+          old_root_taxons.each { |root_taxon| return false unless clone_taxon(root_taxon, terminate: false) }
+          true
         end
 
-        def clone_taxon(parent_taxon)
+        def clone_taxon(parent_taxon, terminate: false)
+          return false if terminate
+
           old_taxons = @old_store.taxons.where(parent: parent_taxon, taxonomy: parent_taxon.taxonomy)
-          return if old_taxons.nil?
+          return false if old_taxons.nil?
 
           new_taxonomy = @new_store.taxonomies.find_by(name: parent_taxon.taxonomy.name)
-          cloned_taxons = clone_update_taxon(old_taxons, new_taxonomy, get_new_root_taxon(new_taxonomy, parent_taxon))
-          return false unless save_models(cloned_taxons)
+          new_parent_taxon = get_new_parent_taxon(new_taxonomy, parent_taxon)
 
-          old_taxons.each { |taxon| clone_taxon(taxon) }
+          cloned_taxons = clone_update_taxon(old_taxons, new_taxonomy, new_parent_taxon)
+          terminate = true unless save_models(cloned_taxons)
+
+          old_taxons.each { |taxon| return false unless clone_taxon(taxon, terminate: terminate) }
+          true
         end
 
         def clone_update_taxon(old_taxons, new_taxonomy, new_parent_taxon)
@@ -81,14 +88,23 @@ module Spree
             new_taxon.parent = new_parent_taxon
             new_taxon
           end
-          taxons = get_model_hash(taxons).map do |taxon|
-            taxon.except('lft', 'rgt', 'depth')
+          attributes_for_each_taxon = get_model_hash(taxons).map do |attributes|
+            attributes.except('lft', 'rgt', 'depth')
           end
-          new_taxonomy.taxons.build(taxons)
+          new_taxonomy.taxons.build(attributes_for_each_taxon)
         end
 
-        def get_new_root_taxon(new_taxonomy, old_parent_taxon)
+        def get_new_parent_taxon(new_taxonomy, old_parent_taxon)
           @new_store.taxons.find_by(permalink: old_parent_taxon.permalink, taxonomy: new_taxonomy)
+        end
+
+        # Menus
+        def handle_clone_menus
+          menus = @old_store.menus.all
+          cloned_menus = @new_store.menus.build(get_model_hash(menus))
+          return false unless save_models(cloned_menus)
+
+          true
         end
 
         # finish lifecycle
