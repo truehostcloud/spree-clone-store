35e67b5aca8983ed9762002b3c7a36936369b146
Collins Lagat <cklagat@gmail.com>
2021-11-18 14:53:25 +0300

    feat: clone menu items

--- app/controllers/spree/olitt/clone_store/clone_store_controller.rb
+++ app/controllers/spree/olitt/clone_store/clone_store_controller.rb
@@ -11,9 +11,15 @@ module Spree
         def test
           @old_store = Spree::Store.find_by(id: source_id_param)
           @new_store = Spree::Store.find_by(id: 4)
-          return unless handle_clone_menus
+          return unless handle_clone_menu_items
 
-          render json: @new_store.menus.all
+          render json: @new_store.menu_items.all
+          # old_menu = @new_store.menus.find_by(id: 13)
+          # parent_menu_item = @old_store.menu_items.find_by(id: 1)
+          # render json: @new_store.menu_items.joins(:menu).find_by(menu: old_menu, name: parent_menu_item.name)
+          # render json: parent_menu_item.parent
+          # render json: @old_store.menu_items.where(parent: parent_menu_item, menu: parent_menu_item.menu)
+          #                        .order(depth: :asc).order(id: :asc)
         end
 
         def clone
@@ -105,6 +111,63 @@ module Spree
           return false unless save_models(cloned_menus)
 
           true
+        end
+
+        # Menu Items
+        def handle_clone_menu_items
+          old_root_menu_items = @old_store.menu_items.where(parent: nil).order(depth: :asc).order(id: :asc)
+          old_root_menu_items.each do |root_menu_item|
+            return false unless clone_menu_item(parent_menu_item: root_menu_item,
+                                                terminate: false)
+          end
+          true
+        end
+
+        def clone_menu_item(parent_menu_item:, terminate: false)
+          return false if terminate
+
+          old_menu_items = @old_store.menu_items.where(parent: parent_menu_item, menu: parent_menu_item.menu)
+                                     .order(depth: :asc).order(id: :asc)
+          return false if old_menu_items.nil?
+
+          cloned_menu_items = clone_menu_item_helper(old_menu_items: old_menu_items, parent_menu_item: parent_menu_item)
+
+          terminate = true unless save_models(cloned_menu_items)
+
+          old_menu_items.each { |menu_item| return false unless clone_menu_item(parent_menu_item: menu_item, terminate: terminate) }
+          true
+        end
+
+        def clone_menu_item_helper(old_menu_items:, parent_menu_item:)
+          new_menu = @new_store.menus.find_by(location: parent_menu_item.menu.location, locale: parent_menu_item.menu.locale)
+          new_parent_menu_item = get_new_parent_menu_item(new_menu: new_menu, old_parent_menu_item: parent_menu_item)
+
+          clone_update_menu_item(old_menu_items: old_menu_items,
+                                 new_menu: new_menu, new_parent_menu_item: new_parent_menu_item)
+        end
+
+        def clone_update_menu_item(old_menu_items:, new_menu:, new_parent_menu_item:)
+          menu_items = old_menu_items.map do |menu_item|
+            new_menu_item = menu_item.dup
+            new_menu_item.parent = new_parent_menu_item
+            new_menu_item
+          end
+          attributes_for_each_taxon = get_model_hash(menu_items).map do |attributes|
+            attributes.except('lft', 'rgt', 'depth')
+          end
+          new_menu.menu_items.build(attributes_for_each_taxon)
+        end
+
+        def get_new_parent_menu_item(new_menu:, old_parent_menu_item:)
+          old_grandparent_menu_item = old_parent_menu_item.parent
+          new_grandparent_menu_item = nil
+          unless old_grandparent_menu_item.nil?
+            new_grandparent_menu_item = @new_store.menu_items.joins(:menu).find_by(menu: new_menu,
+                                                                                   name: old_grandparent_menu_item.name)
+
+          end
+
+          @new_store.menu_items.joins(:menu).find_by(menu: new_menu, name: old_parent_menu_item.name, parent: new_grandparent_menu_item)
         end
 
         # finish lifecycle
