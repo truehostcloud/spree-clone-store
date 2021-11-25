919394d355229dbd02a6a41b9a416f6c4958b2f3
Collins Lagat <cklagat@gmail.com>
2021-11-25 16:23:55 +0300

    feat: taxons wip

--- app/controllers/spree/olitt/clone_store/duplicators/taxons_duplicator.rb
+++ app/controllers/spree/olitt/clone_store/duplicators/taxons_duplicator.rb
@@ -4,49 +4,75 @@ module Spree
       module Duplicators
         class TaxonsDuplicator
           include Spree::Olitt::CloneStore::CloneStoreHelpers
+          attr_reader :errors
 
           def initialize(old_store:, new_store:)
             @old_store = old_store
-            @new_store = Spree::Store.includes(:taxonomies).find_by(id: new_store.id)
+            @new_store = new_store
+
+            @old_taxons_by_parent = @old_store.taxons.includes(:taxonomy, :parent).group_by(&:parent_id)
+            @new_taxonomies_by_name = @new_store.taxonomies.group_by(&:name)
+
+            @new_taxons_cache = @new_store.taxons.group_by(&:permalink)
+            @errors = []
           end
 
           def handle_clone_taxons
-            old_root_taxons = @old_store.taxons.where(parent: nil).order(depth: :asc).order(id: :asc)
-            old_root_taxons.each { |root_taxon| return false unless clone_taxon(root_taxon, terminate: false) }
-            true
+            clone_child_taxons(parent_taxon_id: nil)
           end
 
-          def clone_taxon(parent_taxon, terminate: false)
-            return false if terminate
+          def clone_child_taxons(parent_taxon_id:)
+            return if are_errors_present?
 
-            old_taxons = @old_store.taxons.where(parent: parent_taxon, taxonomy: parent_taxon.taxonomy)
-                                   .order(depth: :asc).order(id: :asc)
-            return false if old_taxons.nil?
+            return unless @old_taxons_by_parent.key?(parent_taxon_id)
 
-            new_taxonomy = @new_store.taxonomies.find_by(name: parent_taxon.taxonomy.name)
-            new_parent_taxon = get_new_parent_taxon(new_taxonomy, parent_taxon)
+            old_child_taxons = get_old_child_taxons(parent_taxon_id: parent_taxon_id)
 
-            cloned_taxons = clone_update_taxon(old_taxons, new_taxonomy, new_parent_taxon)
-            terminate = true unless save_models(cloned_taxons)
+            return old_child_taxons.each { |child_taxon| clone_child_taxons(parent_taxon_id: child_taxon.id) } if parent_taxon_id.nil?
 
-            old_taxons.each { |taxon| return false unless clone_taxon(taxon, terminate: terminate) }
-            true
+            new_child_taxons = reassign_taxon_properies(old_child_taxons: old_child_taxons)
+
+            save_new_taxons(new_child_taxons: new_child_taxons)
+
+            return if are_errors_present?
+
+            old_child_taxons.each { |child_taxon| clone_child_taxons(parent_taxon_id: child_taxon.id) }
           end
 
-          def clone_update_taxon(old_taxons, new_taxonomy, new_parent_taxon)
-            taxons = old_taxons.map do |taxon|
-              new_taxon = taxon.dup
-              new_taxon.parent = new_parent_taxon
-              new_taxon
+          def reassign_taxon_properies(old_child_taxons:)
+            old_child_taxons.map(&:dup).map do |old_taxon|
+              old_taxon.taxonomy = get_new_taxonomy(old_taxon: old_taxon)
+              old_taxon.parent = get_new_parent_taxon(old_taxon: old_taxon)
+              attributes = old_taxon.attributes
+              attributes = attributes.except('lft', 'rgt', 'depth')
+              Spree::Taxon.new attributes
             end
-            attributes_for_each_taxon = get_model_hash(taxons).map do |attributes|
-              attributes.except('lft', 'rgt', 'depth')
-            end
-            new_taxonomy.taxons.build(attributes_for_each_taxon)
           end
 
-          def get_new_parent_taxon(new_taxonomy, old_parent_taxon)
-            @new_store.taxons.find_by(permalink: old_parent_taxon.permalink, taxonomy: new_taxonomy)
+          def save_new_taxons(new_child_taxons:)
+            new_child_taxons.each do |taxon|
+              unless taxon.save
+                @errors << taxon.errors
+                break
+              end
+              @new_taxons_cache[taxon.permalink] = [taxon]
+            end
+          end
+
+          def are_errors_present?
+            !@errors.empty?
+          end
+
+          def get_old_child_taxons(parent_taxon_id:)
+            @old_taxons_by_parent[parent_taxon_id]
+          end
+
+          def get_new_taxonomy(old_taxon:)
+            @new_taxonomies_by_name[old_taxon.taxonomy.name].first
+          end
+
+          def get_new_parent_taxon(old_taxon:)
+            @new_taxons_cache[old_taxon.parent.permalink].first
           end
         end
       end
