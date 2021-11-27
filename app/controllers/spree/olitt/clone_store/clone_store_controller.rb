d581c4332466fdbebf62111508599755ee76089e
Stephen Gachoka <stephengachoka57@gmail.com>
2021-11-27 13:30:56 +0300

    prd

--- app/controllers/spree/olitt/clone_store/clone_store_controller.rb
+++ app/controllers/spree/olitt/clone_store/clone_store_controller.rb
@@ -30,8 +30,9 @@ module Spree
                                                          new_store: @new_store).handle_clone_pages
           return unless Duplicators::SectionsDuplicator.new(old_store: @old_store,
                                                             new_store: @new_store).handle_clone_sections
-          return unless Duplicators::ProductsDuplicator.new(old_store: @old_store,
-                                                            new_store: @new_store).handle_clone_products
+          # return unless Duplicators::ProductsDuplicator.new(old_store: @old_store,
+          #                                                   new_store: @new_store).handle_clone_products
+          return unless handle_clone_products
 
           finish
         end
@@ -98,6 +99,29 @@ module Spree
           store.mail_from_address = mail_from_address
           store.default = false
           store
+        end
+
+        # Products
+        def handle_clone_products
+          old_products = @old_store.products.all
+          new_products = old_products.map { |product| clone_product(old_product: product) }
+
+          return false unless save_models(new_products)
+
+          true
+        end
+
+        def clone_product(old_product:)
+          old_product.dup.tap do |new_product|
+            new_product.taxons = old_product.taxons.all.map { |old_taxon| @new_store.taxons.find_by(permalink: old_taxon.permalink) }
+            new_product.stores = [@new_store]
+            new_product.created_at = nil
+            new_product.deleted_at = nil
+            new_product.updated_at = nil
+            new_product.product_properties = reset_properties(product: old_product)
+            new_product.master = duplicate_master_variant(product: old_product)
+            new_product.variants = old_product.variants.map { |variant| duplicate_variant(variant: variant) }
+          end
         end
 
         # Finish Lifecycle
