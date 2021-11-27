5e63a08f81e6e667cefff2b27ff0077812004936
Stephen Gachoka <stephengachoka57@gmail.com>
2021-11-27 12:22:20 +0300

    prods

--- app/controllers/spree/olitt/clone_store/duplicators/products_duplicator.rb
+++ app/controllers/spree/olitt/clone_store/duplicators/products_duplicator.rb
@@ -8,17 +8,18 @@ module Spree
 
           def initialize(old_store:, new_store:)
             @old_store = old_store
-            @new_store = Spree::Store.includes(:taxonomies).find_by(id: new_store.id)
+            # @new_store = Spree::Store.includes(:taxonomies).find_by(strore_id: new_store.id)
           end
 
           def handle_clone_products
-            @new_store = Spree::Store.includes(:taxons).find_by(id: @new_store.id)
-            new_products = @old_store.products.map { |product| clone_product(old_product: product) }
+            # @new_store = Spree::Store.includes(:taxons).find_by(id: @new_store.id)
+            @old_store.products.map { |product| clone_product(old_product: product) }
 
-            return false unless save_models(new_products)
+            # return false unless save_models(new_products)
 
             true
           end
+
 
           def clone_product(old_product:)
             old_product.dup.tap do |new_product|
@@ -30,6 +31,9 @@ module Spree
               new_product.product_properties = reset_properties(product: old_product)
               new_product.master = duplicate_master_variant(product: old_product)
               new_product.variants = old_product.variants.map { |variant| duplicate_variant(variant: variant) }
+            end
+            unless old_product.save
+              return false
             end
           end
 
