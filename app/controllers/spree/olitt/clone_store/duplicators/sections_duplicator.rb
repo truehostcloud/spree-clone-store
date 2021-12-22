219cd149b037d040893d11368dbffb05d057595c
Collins Lagat <cklagat@gmail.com>
2021-12-22 17:31:28 +0300

    fix: copy images for cms sections

--- app/controllers/spree/olitt/clone_store/duplicators/sections_duplicator.rb
+++ app/controllers/spree/olitt/clone_store/duplicators/sections_duplicator.rb
@@ -23,8 +23,16 @@ module Spree
           def save_section(old_section:)
             new_section = old_section.dup
             new_section.cms_page = @pages_cache[old_section.cms_page.slug].first
+            new_section = duplicate_images(new_section: new_section, old_section: old_section)
             new_section = @linked_resource.assign_linked_resource(model: new_section) unless new_section.linked_resource_id.nil?
             save_model(model: new_section)
+          end
+
+          def duplicate_images(new_section:, old_section:)
+            new_section.image_one.attach(old_section.image_one.blob) unless old_section.image_one.nil?
+            new_section.image_two.attach(old_section.image_two.blob) unless old_section.image_two.nil?
+            new_section.image_three.attach(old_section.image_three.blob) unless old_section.image_three.nil?
+            new_section
           end
         end
       end
