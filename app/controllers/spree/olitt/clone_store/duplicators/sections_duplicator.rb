cc9cd97891fd66d4284c0f329b49fa094e0f1a82
Collins Lagat <cklagat@gmail.com>
2021-12-22 15:59:42 +0300

    fix: `Spree:Taxon` is set by default with the id set to null
    since there's no taxon with an id of `null`, this can lead to an error if section has no link

--- app/controllers/spree/olitt/clone_store/duplicators/sections_duplicator.rb
+++ app/controllers/spree/olitt/clone_store/duplicators/sections_duplicator.rb
@@ -23,7 +23,7 @@ module Spree
           def save_section(old_section:)
             new_section = old_section.dup
             new_section.cms_page = @pages_cache[old_section.cms_page.slug].first
-            new_section = @linked_resource.assign_linked_resource(model: new_section)
+            new_section = @linked_resource.assign_linked_resource(model: new_section) unless new_section.linked_resource_id.nil?
             save_model(model: new_section)
           end
         end
